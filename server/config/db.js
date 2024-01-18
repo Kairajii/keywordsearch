7dba0ad8420cc224b5934dab0d94e8927e63d4ce
Kairajii <tusharkaira456@gmail.com>
2024-01-18 17:50:24 +0530

    added url

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDB = async()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        const conn = await mongoose.connect('mongodb+srv://tusharkaira:tusharkaira123@blog-app.h2gepmu.mongodb.net/keywordsearcher');
         console.log(`Connected to MongoDb Database ${conn.connection.host}`)
     }catch(error){
         console.log(`Error in MongoDb ${error}`);
